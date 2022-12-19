d62e695a403ff6cd49af1fde99846ae3cbe8c142
A8250092 <stanley.mei08@gmail.com>
2022-12-19 21:49:26 +0800

    update

--- Mouse Position Processing/Assets/Scripts/Character.cs
+++ Mouse Position Processing/Assets/Scripts/Character.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Security.Cryptography;
 using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    // save for efficiency
+    float colliderHalfWidth;
+    float colliderHalfHeight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // save for efficiency
+        BoxCollider2D collider = GetComponent<BoxCollider2D>();
+        colliderHalfWidth = collider.size.x / 2;
+        colliderHalfHeight = collider.size.y / 2;
     }
 
     // Update is called once per frame
@@ -22,5 +27,29 @@ public class Character : MonoBehaviour
 
         // move character
         transform.position = position;
+        ClampInScreen();
+    }
+
+    void ClampInScreen()
+    {
+        Vector3 position = transform.position;
+        // clamp horizontally
+        if(position.x - colliderHalfWidth < ScreenUtils.ScreenLeft)
+        {
+            position.x = ScreenUtils.ScreenLeft + colliderHalfWidth;
+        }else if(position.x+ colliderHalfWidth > ScreenUtils.ScreenRight)
+        {
+            position.x = ScreenUtils.ScreenRight - colliderHalfWidth;
+        }
+
+        // clamp vertically
+        if (position.y + colliderHalfHeight > ScreenUtils.ScreenTop)
+        {
+            position.y = ScreenUtils.ScreenTop - colliderHalfHeight;
+        }
+        else if (position.y - colliderHalfHeight < ScreenUtils.ScreenBottom)
+        {
+            position.y = ScreenUtils.ScreenBottom + colliderHalfHeight;
+        }
     }
 }
---
+++ Mouse Position Processing/Assets/Scripts/GameInitializer.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+/// <summary>
+/// Initializes the game
+/// </summary>
+public class GameInitializer : MonoBehaviour
+{
+    /// <summary>
+    /// Awake is called before Start
+    /// </summary>
+	void Awake()
+    {
+        // initialize screen utils
+        ScreenUtils.Initialize();
+    }
+}
