226eb5a0a046e6a0cfc4af87c9a82090afe17af9
A8250092 <stanley.mei08@gmail.com>
2023-01-03 16:59:55 +0800

    update

--- Mouse Position Processing/Assets/Scripts/CharacterChanger.cs
+++ Mouse Position Processing/Assets/Scripts/CharacterChanger.cs
@@ -14,6 +14,9 @@ public class CharacterChanger : MonoBehaviour
     // need for location of the new character
     GameObject currentCharacter;
 
+    // first frame input support
+    bool previousChangeCharacterInput = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -27,36 +30,48 @@ public class CharacterChanger : MonoBehaviour
     void Update()
     {
         // change character on left mouse button
-        if (Input.GetAxis("ChangeCharacter")>0)
+        if (Input.GetAxis("ChangeCharacter") > 0)
         {
-            // save position and destroy current character
-            Vector3 position = currentCharacter.transform.position;
-            Destroy(currentCharacter);
+            if (!previousChangeCharacterInput)
+            {
+                previousChangeCharacterInput=true;
 
-            // instantiate new character
-            int prefabNumber = Random.Range(0, 4);
-            if(prefabNumber == 0)
-            {
-                currentCharacter = Instantiate<GameObject>(
-                    prefabCharacter0, position,
-                    Quaternion.identity);
-            }else if(prefabNumber == 1)
-            {
-                currentCharacter = Instantiate<GameObject>(
-                    prefabCharacter1, position,
-                    Quaternion.identity);
-            }else if(prefabNumber == 2)
-            {
-                currentCharacter = Instantiate<GameObject>(
-                    prefabCharacter2, position,
-                    Quaternion.identity);
+                // save position and destroy current character
+                Vector3 position = currentCharacter.transform.position;
+                Destroy(currentCharacter);
+
+                // instantiate new character
+                int prefabNumber = Random.Range(0, 4);
+                if (prefabNumber == 0)
+                {
+                    currentCharacter = Instantiate<GameObject>(
+                        prefabCharacter0, position,
+                        Quaternion.identity);
+                }
+                else if (prefabNumber == 1)
+                {
+                    currentCharacter = Instantiate<GameObject>(
+                        prefabCharacter1, position,
+                        Quaternion.identity);
+                }
+                else if (prefabNumber == 2)
+                {
+                    currentCharacter = Instantiate<GameObject>(
+                        prefabCharacter2, position,
+                        Quaternion.identity);
+                }
+                else
+                {
+                    currentCharacter = Instantiate<GameObject>(
+                        prefabCharacter3, position,
+                        Quaternion.identity);
+                }
             }
-            else
-            {
-                currentCharacter = Instantiate<GameObject>(
-                    prefabCharacter3, position,
-                    Quaternion.identity);
-            }
+        }
+        else
+        {
+            // no change character input
+            previousChangeCharacterInput = false;
         }
     }
 }
